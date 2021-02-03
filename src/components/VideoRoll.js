7562a592f343e07a08e790400e8b33c0320d9160
Z1mm3r <czimmer@ramapo.edu>
2021-02-02 19:37:46 -0500

    first test

--- src/components/VideoRoll.js
+++ src/components/VideoRoll.js
@@ -6,6 +6,7 @@ import PreviewCompatibleImage from './PreviewCompatibleImage'
 class VideoRoll extends React.Component {
   render() {
     const { data } = this.props
+    console.log(data)
     const { edges: posts } = data.allMarkdownRemark
 
     return (
